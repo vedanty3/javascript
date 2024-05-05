030c2f4915d270cd8ed0ca4b6d6c477baf24e595
vedant <uuvedant4@gmail.com>
2024-05-06 01:03:41 +0530

    chapter 1

---
+++ 00.Hello World/script.js
@@ -0,0 +1,5 @@
+console.log("Hello, I'm Vedant Yetekar!");
+console.log(5 + 6);
+console.log(Math.sqrt(10));
+// multiply and divide has equal priority but whatever comes first will be evaluated first.
+console.log(5 + 6 - (4 / 2) * 4);
