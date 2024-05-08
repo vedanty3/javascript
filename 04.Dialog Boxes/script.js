fbc9c37bd9dc094e9fad1c23d1f3497dfe000b32
vedant <uuvedant4@gmail.com>
2024-05-08 20:49:22 +0530

    chapter 4

---
+++ 04.Dialog Boxes/script.js
@@ -0,0 +1,10 @@
+const alertResult = alert("hello, people of the internet.");
+const isConfirm = confirm("are you vedant yetekar?");
+
+// prompt returns null if input not given.a
+const username = prompt("What's you name?");
+
+console.log(isConfirm);
+console.log(alertResult);
+
+console.log(username);
