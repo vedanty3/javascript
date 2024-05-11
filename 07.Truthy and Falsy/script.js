82ba0e1a483e22099e6af174080b66be06c8c9b0
vedant <uuvedant4@gmail.com>
2024-05-11 14:37:58 +0530

    chapter 7

---
+++ 07.Truthy and Falsy/script.js
@@ -0,0 +1,13 @@
+console.log(Boolean(10));
+console.log(Boolean(-10));
+console.log(Boolean(0));
+console.log(Boolean(Infinity));
+console.log(Boolean(NaN));
+console.log(Boolean(""));
+console.log(Boolean(" "));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean("vedant"));
+console.log(Boolean([]));
+console.log(Boolean(-0));
+console.log(Boolean(!NaN));
