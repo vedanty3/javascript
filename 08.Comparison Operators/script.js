375eae1dbd53b5708609f6a4321564c0f163cf5b
vedant <uuvedant4@gmail.com>
2024-05-12 12:07:15 +0530

    chapter 8

---
+++ 08.Comparison Operators/script.js
@@ -0,0 +1,16 @@
+const user1Age = "18";
+const user2Age = 24;
+const user3Age = 18;
+
+console.log(user1Age == user2Age);
+console.log(user1Age == user3Age);
+console.log(user1Age === user3Age);
+console.log(+user1Age === user3Age);
+console.log(parseInt(user1Age) === user3Age);
+console.log(user3Age != user2Age);
+console.log(user1Age != user3Age);
+console.log(user1Age !== user3Age);
+console.log(user1Age > user2Age);
+console.log(user1Age >= user2Age);
+console.log(user1Age <= user2Age);
+console.log(user1Age < user2Age);
