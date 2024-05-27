60c0b38111fea6b29514bd5ef676442e600cf085
vedant <uuvedant4@gmail.com>
2024-05-28 01:05:39 +0530

    "chapter 10: nested if else"

--- 10.Decision Making/script.js
+++ 10.Decision Making/script.js
@@ -13,4 +13,39 @@ if (userAge >= 18 && userAge <= 21) {
       .concat(username.substring(1, username.length))} is a college student.`
   );
   console.log(`${isMale ? "He" : "She"} is learning CSE.`);
+} else if (userAge < 18 && userAge > 5) {
+  console.log(
+    `${username
+      .charAt(0)
+      .toUpperCase()
+      .concat(username.substring(1, username.length))} is a school student.`
+  );
+} else if (userAge <= 5) {
+  console.log(
+    `${username
+      .charAt(0)
+      .toUpperCase()
+      .concat(username.substring(1, username.length))} is a kid.`
+  );
+} else {
+  console.log(
+    `${username
+      .charAt(0)
+      .toUpperCase()
+      .concat(username.substring(1, username.length))} is an adult.`
+  );
+}
+
+// Nested if-else
+
+if (true) {
+  if (!undefined) {
+    // some code 1...
+  } else {
+    // some code 2...
+  }
+} else if (!NaN) {
+  // some randome code
+} else {
+  // some stupid code
 }
