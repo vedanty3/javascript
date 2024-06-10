3695d7194e73f44dbd029024dd417deb7ac062b2
vedant <uuvedant4@gmail.com>
2024-06-10 23:37:58 +0530

    for loop

---
+++ 22.For Loop/script.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 100; ++i) {
+  console.log(i);
+}
+
+const friends = ["Vedant", "Rahul", "Adarsh", "Gaurav", "John", "Raj"];
+
+for (let i = 0; i < friends.length; ++i) {
+  console.log(friends[i]);
+}
+
+for (let i = 1; i <= 100; ++i) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
