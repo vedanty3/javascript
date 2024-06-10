39d22756e75d34662fb2d01c0317cdcf839adf21
vedant <uuvedant4@gmail.com>
2024-06-10 23:27:55 +0530

    while loop

---
+++ 21.While Loop/script.js
@@ -0,0 +1,15 @@
+let i = 1;
+
+while (i <= 100) {
+  console.log(i);
+  ++i;
+}
+
+const friends = ["Vedant", "Rahul", "Adarsh", "Gaurav", "John", "Raj"];
+
+i = 0;
+
+while (i < friends.length) {
+  console.log(friends[i]);
+  ++i;
+}
