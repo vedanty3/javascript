068b26e0580d26c32633887e62284c272dc7c249
vedant <uuvedant4@gmail.com>
2024-06-14 00:14:21 +0530

    return keyword

--- 24.Functions/script.js
+++ 24.Functions/script.js
@@ -3,7 +3,30 @@ function greet(name = "Vedant Yetekar") {
   return `Hello, How are you ${name}?`;
 }
 
+debugger;
+
 const greeting1 = greet("Parth Yetekar"); // agruement
 const greeting2 = greet(); // agruement
 
 console.log(greeting1, greeting2);
+
+function reverseNumber(number) {
+  let ans = 0;
+
+  while (number) {
+    let lastDigit = Math.round(number % 10);
+    console.log(lastDigit);
+    ans = ans + lastDigit;
+    number = Math.round(number / 10);
+    if (!number) {
+      break;
+    }
+    ans *= 10;
+  }
+
+  return ans;
+}
+
+const reversedNumber = reverseNumber(1234);
+
+console.log(reversedNumber);
