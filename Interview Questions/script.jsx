ee004f92c72317edca8361ed1835f4c3df10d3ee
vedant <uuvedant4@gmail.com>
2024-08-16 01:58:51 +0530

    react questions

---
+++ Interview Questions/script.jsx
@@ -0,0 +1,53 @@
+// example 1
+import { useState, useRef } from "react";
+
+export function App() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button
+        onClick={() => {
+          setCounter((prevState) => prevState + 1);
+        }}
+      >
+        add
+      </button>
+    </div>
+  );
+}
+
+// example 2
+export function App() {
+  const [counter, setCounter] = useState(0);
+  const timerRef = useRef();
+
+  const startInterval = () => {
+    if (timerRef.current) {
+      console.log("timer already started...");
+      return;
+    }
+    timerRef.current = setInterval(() => {
+      setCounter((prevState) => {
+        console.log(prevState);
+        return prevState + 1;
+      });
+    }, 1000);
+  };
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button
+        onClick={() => {
+          startInterval();
+        }}
+      >
+        add
+      </button>
+    </div>
+  );
+}
+
+// 01:30:54
